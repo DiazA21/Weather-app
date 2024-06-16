8d00f5c26a1bdf6b337e15f205d56268ed951c73
Andrew <andrewdiaz1993@gmail.com>
2024-06-16 22:47:12 +0100

    Building app
    Coding the foundation to the app

--- script.js
+++ script.js
@@ -3,23 +3,57 @@ const apiKey = "0a06049708a213c6de1e74f2fe0f162a";
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-// Uses the search fields so that the function can run. When users click the search button, the "checkWeather()" function will run
+// Uses the search fields so that the function can run.
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 // The city is the parameter that the app will search for when the user enters it
 async function checkWeather(city) {
   const response = await fetch(apiURL + city + `&appid=${apiKey}`);
-  var data = await response.json();
 
-  console.log(data);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-//   Selects the "element/class" and updates the text within the HTML element
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
+    console.log(data);
 
+    //  Selects the "element/class" and updates the text within the HTML element
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
-checkWeather();
+// When the user clicks the search button, the city information the user inputs in the the "checkWeather()" function will be called
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value); // the information written in the input field will be passed through the checkWeather() function
+});
+
+searchBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
